2fe92c5139564eea5fbe605bdaf8ef132d994234
mmarkkp <mmarkkp@outlook.com>
2023-12-13 14:03:27 +0800

    Added constructors and attributes to the display and sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,12 @@
 class Display():
-    pass
+    def __init__(self, id, message, is_on, car_park):
+        self.id = id
+        self.message = ""
+        self.is_on = False
+        self.car_park = car_park
+        
+    def __str__(self):
+        return "Display 1: Welcome to the automated car park"
+        
+        
+        
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,14 @@
-class Sensor():
+class Sensor:
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = False
+        self.car_park = car_park
+        
+    def _str__(self):
+        return f'The sensors id is: {self.id} and the status is: {self.is_active}'
+    
+class EntrySensor(Sensor):
+    pass
+
+class ExitSensor(Sensor):
     pass
